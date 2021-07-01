7854267c8c81238bf5ff7b792e7b57c3f4f5592f
Alejandro Noguerón <alex.noguerona@gmail.com>
2021-07-01 18:37:07 +0200

    Updated tutorial, naming, solved dependencies and created empty file sigviewer.py

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="image-viewer-kit-TEST",
-    version="0.4",
+    name="interactive-kit-TEST",
+    version="0.5",
     license='CC0-1.0',
     author="Alejandro Noguerón",
     author_email="alex.noguerona@gmail.com",
@@ -36,15 +36,19 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/Biomedical-Imaging-Group/IPLabImageViewer',
-#     project_urls={
-#         "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-#     },
     classifiers=[
         "Operating System :: OS Independent",
         'Development Status :: 5 - Production/Stable',      # Chose forom "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
         'Programming Language :: Python :: 3.7',
     ],
     package_dir={"": "src"},
+    install_requires=[ 
+          'numpy', 
+          'matplotlib', 
+          'jupyter',
+          'ipympl', 
+          'ipywidgets',
+      ],
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
 )
---
+++ src/interactive_kit/__init__.py
@@ -0,0 +1,4 @@
+from .imviewer import ImageViewer as imviewer
+# from .sigviewer import SignalViewer as sigviewer
+# from .boundary_visualization import BoundaryVisualization as boundviewer
+
