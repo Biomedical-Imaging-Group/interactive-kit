b5e0c6350dd22d977bb443a5e8724131bf44516f
Alejandro Noguerón <alex.noguerona@gmail.com>
2021-07-14 21:15:30 +0200

    Files used to upload to PyPI (link: 'https://pypi.org/project/interactive-kit/0.1rc3/', installation with 'pip install interactive-kit==0.1rc3')

--- setup.py
+++ setup.py
@@ -1,51 +1,29 @@
-# from distutils.core import setup
-# setup(
-#   name = 'interactive-viewer-TEST',         
-#   packages = ['interactive-viewer-TEST'],   
-#   version = '0.2',      
-#   license='CC0-1.0',        
-#   description = 'Interactive image visualization tool, optimized for image comparison, \
-#                  and for applying transforms withing the image',
-#   author = 'BioMedical Imaging Group',
-#   author_email = '',      # Type in your E-Mail
-#   url = 'https://github.com/Biomedical-Imaging-Group/IPLabImageViewer',
-#   download_url = 'https://github.com/Biomedical-Imaging-Group/IPLabImageViewer/archive/v_01.tar.gz',
-#   keywords = ['Image', 'Visualization', 'Intaractivity'],  
-#   install_requires=[ 
-#           'numpy',
-#           'ipywidgets',
-#           'matplotlib'
-#       ],
-#   classifiers=[
-#     'Development Status :: 5 - Production/Stable',      # Chose forom "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
-#     'Programming Language :: Python :: 3.7',
-#   ],
-# )
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="interactive-kit-TEST",
-    version="0.5",
-    license='CC0-1.0',
+    name="interactive-kit",
+    version="0.1rc3",
     author="Alejandro Noguerón",
     author_email="alex.noguerona@gmail.com",
-    description="Interactive image viewer",
+    description="Interactive viewer for signal processing, image processing, and machine learning",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/Biomedical-Imaging-Group/IPLabImageViewer',
+    url='https://github.com/Biomedical-Imaging-Group/interactive-kit',
     classifiers=[
         "Operating System :: OS Independent",
         'Development Status :: 5 - Production/Stable',      # Chose forom "3 - Alpha", "4 - Beta" or "5 - Production/Stable" 
         'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
     ],
     package_dir={"": "src"},
     install_requires=[ 
           'numpy', 
           'matplotlib', 
           'jupyter',
+          'jupyterlab',
           'ipympl', 
           'ipywidgets',
       ],
